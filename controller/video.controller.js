c308fc8951e3268ce71aff2bcbd2f3f7a24eab0e
Shaheer <54540700+shaheerali07@users.noreply.github.com>
2024-11-12 19:03:20 +0500

    exercise added

--- controller/video.controller.js
+++ controller/video.controller.js
@@ -1,9 +1,35 @@
 exports.video_load = async (req, res) => {
-    const path = require("path")
-    const data = req.query
-    const category = data.category
-    const exercise = data.exercise
-    const index = data.index
-    const videoPath = path.join(__dirname, `../sample_video/${index}/${category}`, `${exercise}.mp4`)
-    res.sendFile(videoPath)
-} 
+  const path = require("path");
+  const data = req.query;
+
+  // Decode URL-encoded parameters
+  const category = decodeURIComponent(data.category);
+  const exercise = decodeURIComponent(data.exercise);
+  const index = decodeURIComponent(data.index);
+  const subcategory = data.subcategory
+    ? decodeURIComponent(data.subcategory)
+    : "";
+
+  let videoPath;
+
+  if (subcategory) {
+    videoPath = path.join(
+      __dirname,
+      `../sample_video/${index}/${category}/${subcategory}`,
+      `${exercise}.mp4`
+    );
+  } else {
+    videoPath = path.join(
+      __dirname,
+      `../sample_video/${index}/${category}`,
+      `${exercise}.mp4`
+    );
+  }
+
+  res.sendFile(videoPath, (err) => {
+    if (err) {
+      console.log("Error sending file:", err);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+};
